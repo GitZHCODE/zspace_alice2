aec220f6c2ad650247408712cb2b1359d09cddae
Taizhong Chen <Taizhong.Chen@zaha-hadid.com>
2025-06-28 15:35:24 +0100

    removed all dependencies from git

--- alice2/depends/glfw/src/null_joystick.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ccd7bec0a32632b0fc81d1c085bba3b89e01a9dc4b6b28cde9ea394909fc700d
-size 1424
--- alice2/depends/stb/stb_image_resize2.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:af5fbe1ed423c44cec8155bd0dfea2b5ae6191912790b3a95e15b0b4f610d4df
-size 453371
